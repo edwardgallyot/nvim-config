bd9a86817d5c8fa26ddba8ae2001f061455630d2
montycolonelmustard <77062660+edwardgallyot@users.noreply.github.com>
2024-08-13 10:07:59 +0100

    Tidying keumaps

--- lua/ed/core/keymaps.lua
+++ lua/ed/core/keymaps.lua
@@ -4,14 +4,13 @@ local keymap = vim.keymap
 
 local cmd = vim.cmd
 
-cmd("command! Q q")
-cmd("command! W w")
-
+-- Exit using j and k  
 keymap.set("i", "jk", "<ESC>")
 keymap.set("v", "jk", "<ESC>")
 keymap.set("i", "jk", "<ESC>")
 keymap.set("c", "jk", "<ESC>")
 
+-- Navigate around the screen
 keymap.set("n", "<C-H>", "<C-W>h")
 keymap.set("n", "<C-L>", "<C-W>l")
 keymap.set("n", "<C-K>", "<C-W>k")
@@ -32,7 +31,6 @@ keymap.set('n', '<leader>b',  ":!build\n")
 keymap.set("n", "<leader>+", "\"*y");
 keymap.set("n", "<leader>+", "\"*p");
 
-
 -- Paste last yanked
 keymap.set("n", "<leader>p", "\"0p");
 keymap.set("v", "<leader>p", "\"0p");
@@ -42,20 +40,15 @@ keymap.set("c", "<leader>p", "\"0p");
 keymap.set("n", "<leader>v", "<C-w>v");
 keymap.set("n", "<leader>h", "<C-w>s");
 
+-- Sidebar tree
 keymap.set("n", "<leader>t", ":NvimTreeToggle<CR>");
+
 -- Close split window
 keymap.set("n", "<leader>x", ":close<CR>")
+
 -- Toggle Relative line numbers
 -- https://stackoverflow.com/questions/70432184/is-there-a-way-to-toggle-the-relative-number-setting-in-visual-mode-and-only-for
 keymap.set("n", "<leader>r", ":setlocal relativenumber!<CR>")
-
--- Plugin Keymaps
-keymap.set("n", "<leader>sm", ":MaximizerToggle<CR>")
-
--- From tpope/vim-surround
--- use ysw + " to surround a word with quotes
--- use ysw + ( to surround a wort with bracket
--- use dsw + " to delete double quotes from around a word
 
 -- telescope
 local builtin = require('telescope.builtin')
@@ -65,40 +58,3 @@ keymap.set("n", "<leader>fc", "<cmd>Telescope grep_string<cr>")
 keymap.set("n", "<leader>fb", "<cmd>Telescope buffers<cr>")
 keymap.set("n", "<leader>fh", "<cmd>Telescope help_tags<cr>")
 
--- rust commands
--- keymap.set("n", "<leader>rr", "<cmd>RustRunnables<cr>")
--- keymap.set("n", "<leader>rd", "<cmd>RustDebug<cr>")
--- keymap.set("n", "<leader>ss", "<cmd>mksession! ~/dev/vim_sessions/sesh.vim<cr>");
--- keymap.set("n", "<leader>ll", "<cmd>source ~/dev/vim_sessions/sesh.vim<cr>");
--- keymap.set("n", "<leader>pp", "<cmd>RustParentModule<cr>")
-
--- goyo command
--- keymap.set("n", "<leader>gy", "<cmd>Goyo<cr>")
-
-
--- recommended nvim dap commands
--- keymap.set('n', '<F5>', function() require('dap').continue() end)
--- keymap.set('n', '<F10>', function() require('dap').step_over() end)
--- keymap.set('n', '<F11>', function() require('dap').step_into() end)
--- keymap.set('n', '<F12>', function() require('dap').step_out() end)
--- keymap.set('n', '<Leader>b', function() require('dap').toggle_breakpoint() end)
--- keymap.set('n', '<Leader>B', function() require('dap').set_breakpoint() end)
--- keymap.set('n', '<Leader>lp', function() require('dap').set_breakpoint(nil, nil, vim.fn.input('Log point message: ')) end)
--- keymap.set('n', '<Leader>dr', function() require('dap').repl.open() end)
--- keymap.set('n', '<Leader>dl', function() require('dap').run_last() end)
--- keymap.set({'n', 'v'}, '<Leader>dh', function()
---
---   require('dap.ui.widgets').hover()
--- end)
--- vim.keymap.set({'n', 'v'}, '<Leader>dp', function()
---   require('dap.ui.widgets').preview()
--- end)
--- vim.keymap.set('n', '<Leader>df', function()
---   local widgets = require('dap.ui.widgets')
---   widgets.centered_float(widgets.frames)
--- end)
--- vim.keymap.set('n', '<Leader>ds', function()
---   local widgets = require('dap.ui.widgets')
---   widgets.centered_float(widgets.scopes)
--- end)
---
