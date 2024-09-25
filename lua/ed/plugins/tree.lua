344bce7391f1e1dc4a0dcbb36befc01c99900690
montycolonelmustard <77062660+edwardgallyot@users.noreply.github.com>
2024-09-25 23:22:10 +0100

    finally adding all files to the tree too!

--- lua/ed/plugins/tree.lua
+++ lua/ed/plugins/tree.lua
@@ -1,4 +1,11 @@
 require("nvim-web-devicons").setup()
 
 require("neo-tree").setup({
+  filesystem = {
+    filtered_items = {
+      hide_dotfiles = false,
+      hide_gitignored = false,
+      hide_hidden = false,
+    }
+  }
 })
