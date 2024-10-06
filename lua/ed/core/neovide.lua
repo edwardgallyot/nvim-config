61314aaf89cd8d789e225c6bf750a51ed276986a
montycolonelmustard <edgallyot@gmail.com>
2024-10-06 12:56:57 +0100

    change scale factor

--- lua/ed/core/neovide.lua
+++ lua/ed/core/neovide.lua
@@ -1,5 +1,5 @@
 if vim.g.neovide then
-  vim.g.neovide_scale_factor = 1.0
+  vim.g.neovide_scale_factor = 0.7
 
   local change_scale_factor = function(delta)
     vim.g.neovide_scale_factor = vim.g.neovide_scale_factor * delta
